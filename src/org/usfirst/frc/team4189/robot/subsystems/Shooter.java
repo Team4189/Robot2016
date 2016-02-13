a22b192fdba563d93c048099bdcef1a3e2ac222e
KSalonvaara <kauri@salonvaara.com>
2016-02-13 14:34:45 -0500

    Method Headers
    Added method headers for controlling the shooter.

--- src/org/usfirst/frc/team4189/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team4189/robot/subsystems/Shooter.java
@@ -1,17 +1,36 @@
 package org.usfirst.frc.team4189.robot.subsystems;
 
+import org.usfirst.frc.team4189.robot.commands.DriveWithJoysticks;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Shooter extends Subsystem {
-    
+
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+    public void changeAngleUp() {
+	
+    }
+    
+    public void changeAngleDown() {
+	
+    }
+    
+    public void loadShooter() {
+	
+    }
+    
+    public void fireShooter() {
+	
+    }
+    
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+	// Set the default command for a subsystem here.
+	//setDefaultCommand(new MySpecialCommand());
+	setDefaultCommand(new DriveWithJoysticks());
     }
 }
