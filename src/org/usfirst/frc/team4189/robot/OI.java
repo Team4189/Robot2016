c840aad536b2315ed10da6e4dc581838ce083c18
Will Underwood <wunderwood157@gmail.com>
2016-02-15 13:34:59 -0500

    Cheval Down
    This makes it to where when LStick But. 5 is pressed, it lowers the
    Cheval actuator, but it must be held in order to prevent the isFinished
    method from running. In effect, this allows us to have a whileHeld
    button command that also has an end() method. TL;DR: I’m a genius.

--- src/org/usfirst/frc/team4189/robot/OI.java
+++ src/org/usfirst/frc/team4189/robot/OI.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
+import org.usfirst.frc.team4189.robot.commands.ChevalDown;
 import org.usfirst.frc.team4189.robot.commands.ExampleCommand;
 import org.usfirst.frc.team4189.robot.commands.LifterUp;
 import org.usfirst.frc.team4189.robot.commands.ResetGyro;
@@ -31,36 +32,13 @@ public class OI {
 	public static Button squareUp = new JoystickButton(OI.rightStick , 3);
 	public static Button shooterUp = new JoystickButton(OI.rightStick , 6);
 	public static Button shooterDown = new JoystickButton(OI.rightStick , 4);
+	public static Button chevalDown = new JoystickButton(OI.rightStick , 5);
 	public static Button lifterUp = new JoystickButton(OI.leftStick , 3);
 	public static Button lifterDown = new JoystickButton(OI.leftStick , 2);
 	public static Button winchUp = new JoystickButton(OI.leftStick , 5);
 	
-
-	
-
-
-	
-	
-	
-	
-	
-	//// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
     // Joystick stick = new Joystick(port);
     // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
     // button.whenPressed(new ExampleCommand());
     
     // Run the command while the button is being held down and interrupt it once
@@ -77,6 +55,8 @@ public class OI {
 			winchUp.whenPressed(new WinchUp());
 			shooterUp.whenPressed(new ShooterUp());
 			shooterDown.whenPressed(new ShooterDown());
+			chevalDown.whenPressed(new ChevalDown());
+			
 
 			 //CameraServer.getInstance().startAutomaticCapture(Robot.chassis.pixyCam);
 	 }
